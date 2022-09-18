f81e719195c4f3c8a60851e0e8cc1ed105eabb1a
Md Mamun <juborajvai22@gmail.com>
2022-09-19 00:16:16 +0600

    student route develop

--- Controller/studentController.js
+++ Controller/studentController.js
@@ -35,6 +35,21 @@ exports.getSingleStundetInfo = async (req, res, next) => {
   }
 };
 
+
+// please not font and department case lowarcase set
+exports.getDepartmentStudent = async (req , res , next) =>{
+  const {department} = req.query
+  const departmentOfStudent = await Student.find({department})
+  console.log(departmentOfStudent , department);
+  if(departmentOfStudent.length == 0){
+    res.json({success:false , message: "Thare Are No Deparment Student"})
+
+  }
+  else{
+  res.json({success:true , student: departmentOfStudent})
+  }
+}
+
 exports.searchStudentInformation = async (req, res, next) => {
   try {
     const { educationLevel, department, className, session, classRoll } =
@@ -49,7 +64,7 @@ exports.searchStudentInformation = async (req, res, next) => {
         { classRoll },
       ],
     });
-   
+
     if (student.length == 0) {
       res.status(404).json({
         success: false,
@@ -66,61 +81,50 @@ exports.searchStudentInformation = async (req, res, next) => {
   }
 };
 
-exports.StudentDelete = async (req , res , next) =>{
-  const student = await Student.findById(req.params.id)
+exports.StudentDelete = async (req, res, next) => {
+  const student = await Student.findById(req.params.id);
   if (!student) {
     res.status(404).json({
       success: false,
       message: "Student Not Found",
     });
-  }
-  else{
-    student.remove()
-  res.status(200).json({
-    success: true,
-    message: "Student Delete Successfull",
-  });
-
-  }
-  
-
-}
-
-
-exports.studentInfoUpdate = async (req , res , next) =>{
-  try{
-    let student = await Student.findById(req.params.id);
-  if (!student) {
-    res.status(500).json({
-      success: false,
-      message: "Student Not Fount",
-    });
-  }
-  else{
-    student = await Student.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-      useFindAndModify: false,
-    });
-  
+  } else {
+    student.remove();
     res.status(200).json({
       success: true,
-      student,
+      message: "Student Delete Successfull",
     });
+  }
+};
 
-  }
-  }
-  catch(e){
+exports.studentInfoUpdate = async (req, res, next) => {
+  try {
+    let student = await Student.findById(req.params.id);
+    if (!student) {
+      res.status(500).json({
+        success: false,
+        message: "Student Not Fount",
+      });
+    } else {
+      student = await Student.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+      });
+
+      res.status(200).json({
+        success: true,
+        student,
+      });
+    }
+  } catch (e) {
     console.log(e);
   }
-  
+};
 
-
-
-}
-
-exports.addedStudentResult = async (req , res, next) =>{
-  const {subjectCode , subjectName ,  minMarks , maxMark , grade , examName} = req.body
+exports.addedStudentResult = async (req, res, next) => {
+  const { subjectCode, subjectName, minMarks, maxMark, grade, examName } =
+    req.body;
   let student = await Student.findById(req.params.id);
 
   if (!student) {
@@ -128,18 +132,126 @@ exports.addedStudentResult = async (req , res, next) =>{
       success: false,
       message: "Student Not Fount",
     });
-  }
-  else{
+  } else {
     const result = {
-      subjectCode , subjectName ,  minMarks , maxMark , grade , examName
-    } 
-    student.result.push(result)
+      subjectCode,
+      subjectName,
+      minMarks,
+      maxMark,
+      grade,
+      examName,
+    };
+    student.result.push(result);
     await student.save({ validateBeforeSave: false });
     res.status(200).json({
       success: true,
-      message: "Student Result Publish Successfull"
-    })
+      message: "Student Result Publish Successfull",
+    });
+  }
+};
 
+exports.getStudentResult = async (req, res, next) => {
+  try {
+    const {
+      educationLevel,
+      department,
+      className,
+      session,
+      classRoll,
+      examName,
+    } = req.query;
+
+    let student = await Student.findOne({
+      $and: [
+        { educationLevel: educationLevel },
+        { department },
+        { className },
+        { session },
+        { classRoll },
+      ],
+    });
+
+    if (!student) {
+      res.status(404).json({
+        success: false,
+        message: "Please provide valid student Information",
+      });
+    } else {
+      const studentResult = student.result.filter(
+        (exam) => examName == exam.examName
+      );
+    
+      if (studentResult.length == 0) {
+        res.status(404).json({
+          success: false,
+          message: "Please provide valid student Information",
+        });
+      }
+
+      res.status(200).json({
+        success: true,
+        // student
+        result: studentResult,
+        studentInfo: student,
+      });
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+exports.resultUpdate = async (req , res , next) =>{
+  try {
+    const id = req.params.id
+    const {
+      educationLevel,
+      department,
+      className,
+      session,
+      classRoll,
+      examName,
+    } = req.body;
+
+    let student = await Student.findOne({
+      $and: [
+        { educationLevel: educationLevel },
+        { department },
+        { className },
+        { session },
+        { classRoll },
+      ],
+    });
+
+    if (!student) {
+      res.status(404).json({
+        success: false,
+        message: "Please provide valid student Information",
+      });
+    } else {
+      const studentResult = student.result.filter(
+        (exam) => examName == exam.examName
+      );
+      
+      if (studentResult.length == 0) {
+        res.status(404).json({
+          success: false,
+          message: "Please provide valid student Information",
+        });
+      }
+      const s = student.result.filter((exam) => exam._id == id)
+      console.log(s[0].minMarks = 5);
+      const news = s[0].minMarks = 5
+      await student.save({ validateBeforeSave: false });
+
+      res.status(200).json({
+        success: true,
+        // student
+        result: studentResult,
+        studentInfo: student,
+      });
+    }
+  } catch (e) {
+    console.log(e);
   }
 
 }
--- Router/studentRouter.js
+++ Router/studentRouter.js
@@ -1,6 +1,6 @@
 
 const express = require('express')
-const { studentAdmission, getSingleStundetInfo, getAllStudnt, searchStudentInformation, StudentDelete, studentInfoUpdate, addedStudentResult, getStudentResult } = require('../Controller/studentController')
+const { studentAdmission, getSingleStundetInfo, getAllStudnt, searchStudentInformation, StudentDelete, studentInfoUpdate, addedStudentResult, getStudentResult, getDepartmentStudent, resultUpdate } = require('../Controller/studentController')
 const router = express.Router()
 
 
@@ -12,7 +12,8 @@ router.get("/search" , searchStudentInformation)
 router.delete("/admission/:id" , StudentDelete)
 router.put("/admission/:id" , studentInfoUpdate)
 router.post("/admission/result/:id" , addedStudentResult)
-
+router.get("/deparment/student" , getDepartmentStudent)
+router.put("/result/:id" , resultUpdate)
 
 
 module.exports = router
