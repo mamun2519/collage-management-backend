d2bc600c550117a2dd83870b40cc7f5d4ed5b4ec
Md Mamun <juborajvai22@gmail.com>
2022-11-06 15:30:49 +0600

    update

--- databaseConfig/database.js
+++ databaseConfig/database.js
@@ -1,13 +1,18 @@
 const mongoose = require("mongoose");
-const database = () =>{
-const uri = "mongodb://localhost:27017/collage-Management"
+const database = () => {
+  const uri = `mongodb+srv://mamun:${process.env.DB_PASS}@mamun.jqfhhn5.mongodb.net/?retryWrites=true&w=majority`;
 
-mongoose.connect(uri , { useNewUrlParser: true, useUnifiedTopology: true })
-.then((data) => {
+  // cannet to mongoose
+  mongoose
+    .connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((data) => {
       console.log("mongoose was cannect");
-})
-.catch((error) =>{
-      console.log("this is error",error)
-})
-}
-module.exports = database
+    })
+    .catch((error) => {
+      console.log("this is error", error);
+    });
+};
+module.exports = database;
