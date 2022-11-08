bcf813977fb3bb7597fd717823243205b7e8b05d
Md Mamun <juborajvai22@gmail.com>
2022-11-08 12:22:23 +0600

    update

--- utilitis/sendJwtToken.js
+++ utilitis/sendJwtToken.js
@@ -4,7 +4,7 @@ const sendToken = (user , statusCode , res) =>{
       // const options = {
       //       expires: new Date(
       //         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-      //       ),
+      // ddddddddd      ),
       //       httpOnly: true,
       //     };
         
